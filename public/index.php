988d0587612dff5da5702d24138ae02630f48795
AhmadOthman <trollingosman101@outlook.com>
2024-08-11 21:18:23 +0300

    update 1 file and rename 2 files

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 define('PARENT_DIRECTORY', dirname(__DIR__));
 
+// import composer autoloader
 require_once PARENT_DIRECTORY . '/vendor/autoload.php';
 
 // bootstrap the application
 require_once PARENT_DIRECTORY . "/bootstrap/bootstrap.php";
+
