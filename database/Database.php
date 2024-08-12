7c37c6bf300a4ba52675e0b14c33acee6abe422d
AhmadOthman <trollingosman101@outlook.com>
2024-08-12 14:18:15 +0300

    - Fixed a typo in dsn initialization - Disabled prepares emulation to use php's native prepares - Changed the connetion back to mysql, bugs fixed

--- database/Database.php
+++ database/Database.php
@@ -13,26 +13,23 @@ class Database
 
   protected function __construct()
   {
-    /*
+
     $dsn = Env::get('DB_CONNECTION') . ':';
 
     $dsn .=
       http_build_query(
         data: [
-          "host" => Env::get('DB_HOST'),
+          "host" => Env::get('DB_HOSTNAME'),
           "user" => Env::get('DB_USERNAME'),
           "password" => Env::get('DB_PASSWORD'),
           "dbname" => Env::get('DB_DATABASE'),
-          "charset" => Env::get('DB_CHARSET'),
           "port" => Env::get('DB_PORT'),
+          "charset" => Env::get('DB_CHARSET'),
         ],
         arg_separator: ';'
       );
 
     $this->pdo($dsn);
-    */
-    
-    $this->pdo("sqlite:" . PARENT_DIRECTORY . "/database/database.sqlite");
   }
 
   public static function getInstance(): self
@@ -47,10 +44,14 @@ class Database
   protected static function pdo(string $dsn): PDO
   {
     if (!isset(self::$pdo)) {
-      self::$pdo = new PDO($dsn, options: [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ]);
+      self::$pdo = new PDO(
+        $dsn,
+        options: [
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_EMULATE_PREPARES   => false,
+        ]
+      );
     }
 
     return self::$pdo;
