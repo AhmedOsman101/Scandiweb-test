cb9b57dd662e7b1be393e24b10adada53d8de7e2
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 00:03:05 +0300

    feat(public): Add CORS headers and set JSON content type
    - Set Content-Type header to application/json
    - Added Access-Control-Allow-Origin header for all origins
    - Added Access-Control-Allow-Methods header for common HTTP methods
    - Added Access-Control-Allow-Headers header for Content-Type and Authorization

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 define('PARENT_DIRECTORY', dirname(__DIR__));
 
 // import composer autoloader
