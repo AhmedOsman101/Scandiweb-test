94f1ac324a21a2e42404d97a9742289dc9b6869d
AhmadOthman <trollingosman101@outlook.com>
2024-08-12 23:40:26 +0300

    feat(lib/http): Add Client class for HTTP request handling
    - Created new Client.php file in the lib/http directory
    - Implemented HttpClientInterface to define the execute method contract
    - Developed Client class with singleton pattern for executing HTTP requests:
      - Private constructor to initialize cURL handle
      - getInstance method for singleton implementation
      - execute method to perform HTTP requests with configurable options
    - Included error handling and proper resource management:
      - Try-catch block for exception handling
      - Finally block to reset cURL handle after each request
      - Destructor to close cURL handle when the object is destroyed
    - Integrated with Http class for status message handling

---
+++ lib/http/Client.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Lib\Http;
+
+interface HttpClientInterface
+{
+  public function execute(string $method, string $url, array $options = []): Response;
+}
+
+class Client implements HttpClientInterface
+{
+  private static ?Client $instance = null;
+  private $ch;
+
+  private function __construct()
+  {
+    $this->ch = curl_init();
+  }
+
+  public static function getInstance(): self
+  {
+    if (self::$instance === null) {
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
+
+  public function execute(string $method, string $url, array $options = []): Response
+  {
+    $allOptions = [
+      CURLOPT_URL => $url,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_RETURNTRANSFER => true,
+    ];
+
+    if (!empty($options['headers'])) {
+      $allOptions[CURLOPT_HTTPHEADER] = $options['headers'];
+    }
+
+    if (!empty($options['return'])) {
+      $allOptions[CURLOPT_RETURNTRANSFER] = $options['return'];
+    }
+
+    if (!empty($options['data'])) {
+      $allOptions[CURLOPT_POSTFIELDS] = json_encode($options['data']);
+    }
+
+    curl_setopt_array($this->ch, $allOptions);
+
+    try {
+      $response = curl_exec($this->ch);
+
+      $status = [];
+
+      if ($response === false) {
+        $status["error"] =  curl_error($this->ch);
+      }
+
+      $statusCode = curl_getinfo($this->ch, CURLINFO_RESPONSE_CODE);
+
+      if (in_array($statusCode, array_keys(Http::STATUS_MESSAGES))) {
+        $status["response"]  = Http::STATUS_MESSAGES[$statusCode];
+      }
+
+      return new Response($response, $statusCode, $status);
+    } catch (\Throwable $e) {
+      throw new \RuntimeException("HTTP request failed: " . $e->getMessage());
+    } finally {
+      curl_reset($this->ch);
+    }
+  }
+
+  public function __destruct()
+  {
+    curl_close($this->ch);
+  }
+}
