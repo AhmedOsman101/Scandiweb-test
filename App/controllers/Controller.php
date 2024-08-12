397ce43ed100039d3ea2c9697f0830d1caeef75a
AhmadOthman <trollingosman101@outlook.com>
2024-08-12 23:52:55 +0300

    refactor(controllers): Convert Controller class to interface
    - Changed Controller from an abstract class to an interface
    - Removed unused methods:
      - create()
      - view()
    - Kept essential CRUD methods in the interface:
      - index()
      - store()
      - destroy()
    - Removed method bodies, as interfaces only define method signatures
    - Removed use statement for Lib\Helpers as it's no longer needed

--- App/controllers/Controller.php
+++ App/controllers/Controller.php
@@ -2,33 +2,20 @@
 
 namespace App\Controllers;
 
-use Lib\Helpers;
-
-abstract class Controller
+interface Controller
 {
   /**
    * Retrieve all resources
    */
-  public static function index() {}
-
-  /**
-   * Display a form to create new resources
-   */
-  public static function create() {}
+  public static function index();
 
   /**
    * Create new resources
    */
-  public static function store() {}
+  public static function store();
 
   /**
    * Delete resources
    */
-  public static function destroy() {}
-
-  protected static function view(string $view, $data = [])
-  {
-    if (count($data)) extract($data);
-    return require_once Helpers::base_path("views/$view.view.php");
-  }
+  public static function destroy();
 }
