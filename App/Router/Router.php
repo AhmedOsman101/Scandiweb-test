f3c6b6183c37de003a051b4fda175e2f220ccb6d
AhmadOthman <trollingosman101@outlook.com>
2024-08-12 15:42:39 +0300

    refactor: enhance Router class with additional HTTP method handlers and refactor route storage
    - Replaced private $routes property with public and initialized it as an empty array.
    - Added methods for handling various HTTP methods: get, post, delete, put, and patch.
    - Refactored add method to support named routes and handle both named and unnamed routes using compact.
    - Updated watch method to determine the request method via POST `_method` or SERVER REQUEST_METHOD.
    - Adjusted route matching logic to check against the current request URI and method.
    - Added `get_route` method to retrieve the URI of a named route.

--- App/Router/Router.php
+++ App/Router/Router.php
@@ -4,19 +4,65 @@ namespace App\Router;
 
 class Router
 {
-  private $routes;
+  private array $routes = [];
 
-  public function __construct(array $routes)
+  public function add(string $method, string $uri, mixed $action, string $name = null): void
   {
-    $this->routes = $routes;
+    if ($name) $this->routes[$name] = compact(
+      'method',
+      'uri',
+      'action'
+    );
+    else $this->routes[] = compact(
+      'method',
+      'uri',
+      'action'
+    );
   }
 
-  public function watch(string $uri, string $method)
+  public function get(string $uri, mixed $action, string $name = null): void
   {
+    $this->add("GET", $uri, $action, $name);
+  }
+
+  public function post(string $uri, mixed $action, string $name = null): void
+  {
+    $this->add("POST", $uri, $action, $name);
+  }
+
+  public function delete(string $uri, mixed $action, string $name = null): void
+  {
+    $this->add("DELETE", $uri, $action, $name);
+  }
+
+  public function put(string $uri, mixed $action, string $name = null): void
+  {
+    $this->add("PUT", $uri, $action, $name);
+  }
+
+  public function patch(string $uri, mixed $action, string $name = null): void
+  {
+    $this->add("PATCH", $uri, $action, $name);
+  }
+
+  public function watch()
+  {
+    // Extract the current URI
+    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+    // Current request method is determined by a hidden input
+    // with the name _method or the request method header
+    $request_method = $_POST["_method"] ?? $_SERVER['REQUEST_METHOD'];
+
     foreach ($this->routes as $route) {
-      if ($route['uri'] === $uri && $route['method'] === $method) {
+      if ($route['uri'] === $uri && $route['method'] === $request_method) {
         call_user_func($route['action']);
       }
     }
+  }
+
+  public function get_route(string $name): string|null
+  {
+    return $this->routes[$name]['uri'] ?? null;
   }
 }
