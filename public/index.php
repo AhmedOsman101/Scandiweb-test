2b1a28fd71e2378e687118c64ebaf60f2ea4ab16
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 20:38:28 +0300

    Updated headers

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 header("Content-type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
 define('PARENT_DIRECTORY', dirname(__DIR__));
