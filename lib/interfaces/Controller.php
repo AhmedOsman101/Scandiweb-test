21d8a02e8b756fd2007ac388e1758f100be49861
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 23:51:42 +0300

    refactor(lib): Convert Controller interface to abstract class
    - Changed Controller from interface to abstract class
    - Added empty method implementations for index(), store(), and destroy()
    - Introduced view() method for rendering views:
      - Extracts data array to variables
      - Uses Helpers::base_path() to locate view files
    - Added use statement for Lib\Helpers

--- lib/interfaces/Controller.php
+++ lib/interfaces/Controller.php
@@ -2,20 +2,28 @@
 
 namespace Lib\Interfaces;
 
-interface Controller
+use Lib\Helpers;
+
+abstract class Controller
 {
   /**
    * Retrieve all resources
    */
-  public static function index();
+  public static function index() {}
 
   /**
    * Create new resources
    */
-  public static function store();
+  public static function store() {}
 
   /**
    * Delete resources
    */
-  public static function destroy();
+  public static function destroy() {}
+
+  public static function view(string $view, $data = [])
+  {
+    if (count($data)) extract($data);
+    return require_once Helpers::base_path("views/$view.view.php");
+  }
 }
