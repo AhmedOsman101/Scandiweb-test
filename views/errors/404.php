063d31814121663f08ffb625b745006da3db5373
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 23:46:06 +0300

    feat: create add.view.php, 404.php and views/index.view.php

---
+++ views/errors/404.php
@@ -0,0 +1,52 @@
+<?php
+
+use Lib\Helpers;
+?>
+<!DOCTYPE html>
+<html
+	lang="en"
+	class="dark">
+	<head>
+		<title>Products List</title>
+
+		<meta charset="utf-8" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1" />
+
+		<link
+			rel="stylesheet"
+			href="/assets/sass/main.css" />
+
+		<script
+			defer
+			src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+		<script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+	</head>
+
+	<body class="bg-gray-950 min-h-dvh text-white">
+		<section
+			class="flex items-center justify-center flex-col h-full max-h-dvh p-16 text-gray-400">
+			<div
+				class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+				<div class="max-w-md text-center">
+					<h2 class="mb-8 font-extrabold text-9xl text-gray-400">
+						<span class="sr-only">Error</span>404
+					</h2>
+					<p class="text-2xl font-semibold md:text-3xl">
+						Sorry, we couldn't find this page.
+					</p>
+					<p class="mt-4 mb-8 text-gray-600">
+						But dont worry, you can find plenty of other things on
+						our homepage.
+					</p>
+					<a
+						href="<?= Helpers::route('product.index') ?>"
+						class="px-8 py-3 font-semibold rounded bg-blue-600 text-gray-50"
+						>Back to homepage</a
+					>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
---
+++ views/index.view.php
@@ -0,0 +1,81 @@
+<?php $nav_title = "Products List (" . count($products) . ")" ?>
+
+<!DOCTYPE html>
+<html
+  lang="en"
+  class="dark">
+
+<head>
+  <title>Products List</title>
+
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1" />
+
+  <link
+    rel="stylesheet"
+    href="/assets/sass/main.css" />
+
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+</head>
+
+<body class="bg-gray-950 min-h-dvh text-white">
+  <header class="py-6">
+    <nav class="flex justify-between px-7">
+      <h1 class="font-bold text-2xl">
+        <?= $nav_title ?>
+      </h1>
+
+      <div class="flex space-x-6">
+        <a
+          href="add-product"
+          class="bg-gray-600 rounded-sm px-3 py-1.5 hover:bg-blue-600 transition-colors duration-300">
+          ADD
+        </a>
+        <button
+          class="bg-gray-600 rounded-sm px-3 py-1.5 hover:bg-red-800 transition-colors duration-300"
+          id="delete-product-btn">
+          MASS DELETE
+        </button>
+      </div>
+    </nav>
+  </header>
+  <hr />
+  <main class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-7 mt-8 px-6">
+    <?php foreach ($products as $product): ?>
+      <div
+        class="border p-4 flex flex-col gap-3 bg-gray-800 items-center">
+        <input
+          type="checkbox"
+          name="check"
+          class="delete-checkbox self-start ml-4 rounded" />
+
+        <h2>
+          <span>Name:</span>
+          <?= htmlspecialchars($product->name) ?>
+        </h2>
+
+        <p>
+          <span>SKU:</span> <?= htmlspecialchars($product->sku) ?>
+        </p>
+
+        <p>Price: $<?= number_format($product->price, 2) ?></p>
+      </div>
+
+      </div>
+    <?php endforeach; ?>
+
+  </main>
+  <hr class="mt-8" />
+  <footer class="py-8 grid place-items-center">
+    <p class="text-center font-semibold">
+      Scandiweb Test Assignment &copy; 2024
+    </p>
+  </footer>
+</body>
+
+</html>
