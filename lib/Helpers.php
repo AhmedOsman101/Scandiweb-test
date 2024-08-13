1ac6581dcf4feb108167962318d6c0d1e41b301f
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 23:45:19 +0300

    refactor(lib): Enhance Helpers class with improved functionality and documentation
    - Added PHPDoc comments to all methods for better code documentation
    - Updated dd() method to use static::dump() for consistency
    - Improved type hinting for $router property
    - Added new redirect() method for easy route-based redirections
    - Updated base_path() method parameter to be explicitly typed as string
    - Enhanced code readability and maintainability through consistent naming and formatting

--- lib/Helpers.php
+++ lib/Helpers.php
@@ -4,17 +4,32 @@ namespace Lib;
 
 use App\Router\Router;
 
+/**
+ * The Helpers class provides a set of utility functions globally available across the application.
+ */
 class Helpers
 {
 
-  protected static $router;
+  protected static Router $router;
 
+  /**
+   * Dumps the provided data to the browser in a formatted way then kills the script execution.
+   *
+   * @param mixed ...$data The data to be dumped.
+   * @return void
+   */
   public static function dd(...$data): void
   {
-    self::dump(...$data);
+    static::dump(...$data);
     die;
   }
 
+  /**
+   * Dumps the provided data to the browser in a formatted way.
+   *
+   * @param mixed ...$data The data to be dumped.
+   * @return void
+   */
   public static function dump(...$data): void
   {
     foreach ($data as $item) {
@@ -24,16 +39,47 @@ class Helpers
     }
   }
 
+  /**
+   * Returns the route URI for the given route name.
+   *
+   * @param string $name The name of the route.
+   * @return string|null The route URI, or null if the route is not found.
+   */
   public static function route(string $name): string|null
   {
     return static::$router->get_route($name);
   }
 
-  public static function base_path($path = ''): string
+  /**
+   * Redirects the user to the specified route.
+   *
+   * @param string $to The name of the route to redirect to.
+   * @return void
+   */
+  public static function redirect(string $to): void
+  {
+    $uri = static::route($to);
+    header("Location: $uri");
+    exit;
+  }
+
+  /**
+   * Returns the base path for the application with a given path appended.
+   *
+   * @param string $path The path to append to the base path.
+   * @return string The base path, with the path appended.
+   */
+  public static function base_path(string $path): string
   {
     return PARENT_DIRECTORY . '/' . $path;
   }
 
+  /**
+   * Sets the router instance to be used by the Helpers class.
+   *
+   * @param Router $router The router instance to be used.
+   * @return void
+   */
   public static function set_router(Router $router): void
   {
     static::$router = $router;
