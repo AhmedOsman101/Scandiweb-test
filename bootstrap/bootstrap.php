ee0d11e6f23ce7434d14a7dfe9b97a1baa648c1f
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 14:21:54 +0300

    - Removed unsed config

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -6,8 +6,5 @@ use Lib\Helpers;
 // load environment variables on app start
 Env::load(Helpers::base_path(".env"));
 
-// load app config
-$config = require_once Helpers::base_path("config/config.php");
-
 // init the router
 require_once Helpers::base_path("routes/routes.php");
