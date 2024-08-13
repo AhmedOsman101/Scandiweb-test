d92f7eed5db5bd75e9fc871c0067a054d2112af6
AhmadOthman <trollingosman101@outlook.com>
2024-08-13 23:49:18 +0300

    revert(public): Remove CORS and JSON headers from index.php
    - Removed Content-Type header for JSON responses
    - Removed Access-Control-Allow-Origin header
    - Removed Access-Control-Allow-Headers header
    - Removed Access-Control-Allow-Methods header
    - Reverted the application back to handling traditional web requests instead of acting as a JSON API

--- public/index.php
+++ public/index.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-header("Content-type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-
-
 define('PARENT_DIRECTORY', dirname(__DIR__));
 
 // import composer autoloader
