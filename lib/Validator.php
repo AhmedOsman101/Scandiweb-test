11341d2506a7a03639e39bc3ade8d415580f649a
AhmadOthman <trollingosman101@outlook.com>
2024-08-14 20:18:24 +0300

    refactor(Validator): Improve validation logic and extend validation methods
    - Enhanced error handling in `validateStringLength()` method:
      - Added more descriptive error message for the string length validation
      - Replaced ternary operator with if-else statement for better readability
    
    - Modified `between()` method:
      - Renamed parameters for better clarity (e.g., `value` to `number`)
      - Added `$field_name` as an optional parameter to set error messages
      - Refactored logic to set error messages based on range boundaries (e.g., `-INF` and `INF`)
      - Updated to return errors if validation fails
    
    - Added new `float()` method:
      - Implemented validation to check if a value is a valid float
      - Included optional range validation using the `between()` method
      - Added error messages for invalid float values and out-of-range conditions
    
    - Added new `int()` method:
      - Implemented validation to check if a value is a valid integer
      - Included optional range validation using the `between()` method
      - Added error messages for invalid integer values and out-of-range conditions

--- lib/Validator.php
+++ lib/Validator.php
@@ -26,23 +26,93 @@ class Validator
             static::between($len, $min, $max); // validate the string between the requested range
 
         if (!$is_valid) {
-            $this->errors[$field_name] =
-                ($len === 0)
-                ? "$field_name is required"
-                : "$field_name must be between $min and $max";
+            if ($len === 0) {
+                $this->errors[$field_name] = "$field_name is required";
+            } else {
+                $this->errors[$field_name] = "$field_name must be between $min and $max characters long";
+            }
         }
     }
 
     /**
-     * Checks if a given value is between the specified minimum and maximum values.
+     * Validates that a given value is between the specified minimum and maximum values.
      *
-     * @param float $value The value to check.
-     * @param float $min The minimum value (inclusive).
+     * @param string|null $field_name The name of the field being validated. If
+     * provided, an error message will be added to the $errors array if the
+     * value is not within the specified range.
+     * @param float $number The value to validate.
+     * @param float $min The minimum value (inclusive). Defaults to negative infinity.
      * @param float $max The maximum value (inclusive). Defaults to positive infinity.
-     * @return bool True if the value is between the minimum and maximum, false otherwise.
+     * @return bool True if the value is within the specified range, false otherwise.
      */
-    public static function between(float $value, float $min, float $max = INF): bool
+    public function between(string $field_name = null, float $number, float $min = -INF, float $max = INF): bool
     {
-        return $min <= $value && $value <= $max;
+        $is_between_range = $min <= $number && $number <= $max;
+
+        if (!$is_between_range && $field_name) {
+            $this->errors[$field_name] = "$field_name must be between $min and $max";
+
+            if ($min === -INF) {
+                $this->errors[$field_name] = "$field_name must not be bigger than $max";
+            } else if ($max === INF) {
+                $this->errors[$field_name] = "$field_name must be at least $min";
+            }
+        }
+
+        return $is_between_range;
+    }
+
+
+    /**
+     * Validates that a given value is a float and optionally between the
+     * specified minimum and maximum values.
+     *
+     * @param string $field_name The name of the field being validated.
+     * @param float $number The float value to validate.
+     * @param float $min The minimum value (inclusive). Defaults to negative infinity.
+     * @param float $max The maximum value (inclusive). Defaults to positive infinity.
+     * @return bool True if the value is valid, false otherwise. This method will
+     * add an error to the $errors array if the value is invalid.
+     */
+    public function float(string $field_name, float $number, float $min = -INF, float $max = INF): bool
+    {
+        $is_float = filter_var($number, FILTER_VALIDATE_FLOAT) && true;
+
+        if (!$is_float) {
+            // set the an error if not a float
+            $this->errors[$field_name] = "$field_name must be a decimal value";
+            // early return
+            return false;
+        }
+
+        // check if it's between range, set errors if any then return
+        return static::between($field_name, $number, $min, $max);
+    }
+
+
+    /**
+     * Validates that a given value is an integer and optionally between the
+     * specified minimum and maximum values.
+     *
+     * @param string $field_name The name of the field being validated.
+     * @param int $number The integer value to validate.
+     * @param int $min The minimum value (inclusive). Defaults to negative infinity.
+     * @param int $max The maximum value (inclusive). Defaults to positive infinity.
+     * @return bool True if the value is valid, false otherwise. This method will
+     * add an error to the $errors array if the value is invalid.
+     */
+    public function int(string $field_name, int $number, int $min = -INF, int $max = INF): bool
+    {
+        $is_int = filter_var($number, FILTER_VALIDATE_INT) && true;
+
+        if (!$is_int) {
+            // set the an error if not a integer
+            $this->errors[$field_name] = "$field_name must be an integer value";
+            // early return
+            return false;
+        }
+
+        // check if it's between range, set errors if any and return
+        return static::between($field_name, $number, $min, $max);
     }
 }
