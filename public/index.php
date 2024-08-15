d7c7fa04d76bb3bcbdc3a5a71ab3fc042db642b4
AhmadOthman <trollingosman101@outlook.com>
2024-08-15 20:19:10 +0300

    feat: integrate flash message handling in application entry point
    - Added `Flash::flush()` to the application entry point (`public/index.php`).
    - Ensures any remaining flash messages are cleared at the start of each request.
    - Initiated the session using `session_start()` to manage session data effectively.

--- public/index.php
+++ public/index.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
+use App\Sessions\Flash;
+
 define('PARENT_DIRECTORY', dirname(__DIR__));
+
+session_start();
 
 // import composer autoloader
 require_once PARENT_DIRECTORY . '/vendor/autoload.php';
 
 // bootstrap the application
 require_once PARENT_DIRECTORY . "/bootstrap/bootstrap.php";
+
+// flush any remaining flash messages
+Flash::flush();
