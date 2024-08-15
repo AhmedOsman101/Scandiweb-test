fcec142c356f30ca5a362efdc06b07ca7e0db40f
AhmadOthman <trollingosman101@outlook.com>
2024-08-15 15:48:16 +0300

    refactor: enhance validation methods and error handling in Validator class
    - Refactored `string`, `float`, and `int` methods to include optional parameter checks and improved error handling.
    - Modified `between` method to support mixed data types and enhanced range validation.
    - Introduced `in_enum` method for validating values against PHP enums.
    - Added utility methods: `optional` to handle optional fields, `get_errors`, `has_errors`, and `get_error` for error management.
    - Replaced INF constants with PHP_INT_MIN, PHP_INT_MAX, PHP_FLOAT_MIN, and PHP_FLOAT_MAX for better range management.
    - Improved readability and maintainability by simplifying error setting logic.

--- lib/Validator.php
+++ lib/Validator.php
@@ -7,59 +7,76 @@ class Validator
     protected array $errors = [];
 
     /**
-     * Validates that a given string is between the specified minimum and maximum lengths.
+     * Validates a string based on the provided parameters.
      *
-     * @param string $field_name The name of the field being validated.
-     * @param string $string The string to validate.
-     * @param int $min The minimum length of the string (inclusive).
-     * @param int $max The maximum length of the string (inclusive).
-     * @return void This method does not return a value, but will add an error to the $errors array if the string is invalid.
+     * @param string $field The name of the field being validated.
+     * @param mixed $string The string value to validate.
+     * @param int $min The minimum length of the string (default is 1).
+     * @param int $max The maximum length of the string (default is PHP_INT_MAX).
+     * @param bool $optional Whether the field is optional (default is false).
+     *
+     * @return void Adds an error message to the `$errors` array if validation fails.
      */
-    public function string(string $field_name, string $string, int $min = 1, int $max = INF): void
-    {
+    public function string(
+        string $field,
+        mixed $string,
+        int $min = 1,
+        int $max = PHP_INT_MAX,
+        bool $optional = false
+    ) {
+        if ($this->optional($optional, $string)) return;
+
+        if (!is_string($string)) {
+            return $this->set_error($field, "must be a string.");
+        }
+
         $len = strlen(trim($string));
 
-        $is_valid =
+        if ($len === 0 && !$optional) {
+            return $this->set_error($field, "is required.");
+        }
+
+        $is_in_range =
             0 < $min    && // validate minimum is bigger than zero
             $min < $max && // validate max is bigger than min
             0 < $len    && // validate string isn't empty
-            static::between($len, $min, $max); // validate the string between the requested range
+            $this->between($field, $len, $min, $max); // validate the string between the requested range
 
-        if (!$is_valid) {
-            if ($len === 0) {
-                $this->errors[$field_name] = "$field_name is required";
-            } else {
-                $this->errors[$field_name] = "$field_name must be between $min and $max characters long";
-            }
+        if (!$is_in_range) {
+            $this->errors[$field] .= " characters long.";
         }
     }
 
     /**
      * Validates that a given value is between the specified minimum and maximum values.
      *
-     * @param string|null $field_name The name of the field being validated. If
+     * @param string|null $field The name of the field being validated. If
      * provided, an error message will be added to the $errors array if the
      * value is not within the specified range.
-     * @param float $number The value to validate.
+     * @param mixed $number The value to validate.
      * @param float $min The minimum value (inclusive). Defaults to negative infinity.
      * @param float $max The maximum value (inclusive). Defaults to positive infinity.
      * @return bool True if the value is within the specified range, false otherwise.
      */
-    public function between(string $field_name = null, float $number, float $min = -INF, float $max = INF): bool
-    {
-        $is_between_range = $min <= $number && $number <= $max;
+    public function between(
+        string $field,
+        mixed $number,
+        float $min = PHP_FLOAT_MIN,
+        float $max = PHP_FLOAT_MAX,
+    ): bool {
+        $is_in_range = $min <= $number && $number <= $max;
 
-        if (!$is_between_range && $field_name) {
-            $this->errors[$field_name] = "$field_name must be between $min and $max";
+        if (!$is_in_range) {
+            $this->errors[$field] = "$field must be between $min and $max";
 
-            if ($min === -INF) {
-                $this->errors[$field_name] = "$field_name must not be bigger than $max";
-            } else if ($max === INF) {
-                $this->errors[$field_name] = "$field_name must be at least $min";
+            if ($min === PHP_FLOAT_MIN || PHP_INT_MIN) {
+                $this->errors[$field] = "$field must not be more than $max";
+            } else if ($max === PHP_FLOAT_MAX || PHP_INT_MAX) {
+                $this->errors[$field] = "$field must be at least $min";
             }
         }
 
-        return $is_between_range;
+        return $is_in_range;
     }
 
 
@@ -67,26 +84,32 @@ class Validator
      * Validates that a given value is a float and optionally between the
      * specified minimum and maximum values.
      *
-     * @param string $field_name The name of the field being validated.
-     * @param float $number The float value to validate.
+     * @param string $field The name of the field being validated.
+     * @param mixed $number The float value to validate.
      * @param float $min The minimum value (inclusive). Defaults to negative infinity.
      * @param float $max The maximum value (inclusive). Defaults to positive infinity.
-     * @return bool True if the value is valid, false otherwise. This method will
+     * @param bool $optional Whether the field is optional (default is false).
+     * @return void True if the value is valid, false otherwise. This method will
      * add an error to the $errors array if the value is invalid.
      */
-    public function float(string $field_name, float $number, float $min = -INF, float $max = INF): bool
-    {
+    public function float(
+        string $field,
+        mixed $number,
+        float $min = PHP_FLOAT_MIN,
+        float $max = PHP_FLOAT_MAX,
+        bool $optional = false
+    ) {
+
+        if ($this->optional($optional, $number)) return;
+
         $is_float = filter_var($number, FILTER_VALIDATE_FLOAT) && true;
 
         if (!$is_float) {
-            // set the an error if not a float
-            $this->errors[$field_name] = "$field_name must be a decimal value";
-            // early return
-            return false;
+            return $this->set_error($field, "must be a decimal value.");
         }
 
-        // check if it's between range, set errors if any then return
-        return static::between($field_name, $number, $min, $max);
+        // check if it's between range and set errors if any.
+        $this->between($field, $number, $min, $max);
     }
 
 
@@ -94,25 +117,68 @@ class Validator
      * Validates that a given value is an integer and optionally between the
      * specified minimum and maximum values.
      *
-     * @param string $field_name The name of the field being validated.
-     * @param int $number The integer value to validate.
+     * @param string $field The name of the field being validated.
+     * @param mixed $number The integer value to validate.
      * @param int $min The minimum value (inclusive). Defaults to negative infinity.
      * @param int $max The maximum value (inclusive). Defaults to positive infinity.
-     * @return bool True if the value is valid, false otherwise. This method will
+     * @param bool $optional Whether the field is optional (default is false).
+     * @return void True if the value is valid, false otherwise. This method will
      * add an error to the $errors array if the value is invalid.
      */
-    public function int(string $field_name, int $number, int $min = -INF, int $max = INF): bool
-    {
+    public function int(
+        string $field,
+        mixed $number,
+        int $min = PHP_INT_MIN,
+        int $max = PHP_INT_MAX,
+        bool $optional = false
+    ) {
+
+        if ($this->optional($optional, $number)) return;
+
         $is_int = filter_var($number, FILTER_VALIDATE_INT) && true;
 
         if (!$is_int) {
             // set the an error if not a integer
-            $this->errors[$field_name] = "$field_name must be an integer value";
-            // early return
-            return false;
+            return $this->set_error($field, "must be an integer value.");
         }
 
         // check if it's between range, set errors if any and return
-        return static::between($field_name, $number, $min, $max);
+        $this->between($field, $number, $min, $max);
+    }
+
+    public function in_enum(string $field, mixed $value, string $enum): void
+    {
+        $is_valid = $enum::tryFrom($value);
+        $is_valid = call_user_func([$enum, "tryFrom"], $value);
+
+        if (!$is_valid) {
+            $validValues = implode(', ', array_map(fn($case) => $case->value, call_user_func([$enum, "cases"])));
+            $this->errors[$field] = "Invalid value for $field. The value must be one of the following: $validValues.";
+        }
+    }
+
+    private function optional(bool $optional, mixed $value)
+    {
+        return ($value === null || $value === "") & $optional;
+    }
+
+    public function get_errors(): array
+    {
+        return $this->errors;
+    }
+
+    public function has_errors(): bool
+    {
+        return (bool) sizeOf($this->errors);
+    }
+
+    public function get_error(string $field): string|null
+    {
+        return $this->errors[$field] ?? null;
+    }
+
+    private function set_error(string $field, string $message): void
+    {
+        $this->errors[$field] = "$field $message";
     }
 }
