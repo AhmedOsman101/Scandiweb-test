74819ed5140928b6aaa82facd46e60eb48acdf9e
AhmadOthman <trollingosman101@outlook.com>
2024-08-15 18:10:55 +0300

    - Initialized flash messages in the $_SESSION

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Sessions\Flash;
+use App\Sessions\Session;
 use Lib\Env;
 use Lib\Helpers;
 
 // load environment variables on app start
 Env::load(Helpers::base_path(".env"));
+
+// init flash messages session key
+Session::set(Flash::FLASH_KEY, []);
 
 // init the router
 require_once Helpers::base_path("routes/routes.php");
