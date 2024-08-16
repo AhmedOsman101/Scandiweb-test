6e587a69e8026e579340d8f281e90f6da7e7db20
AhmadOthman <trollingosman101@outlook.com>
2024-08-16 17:58:54 +0300

    refactor: reposition session_start() call for clarity
    - Moved `session_start()` to be called before the `PARENT_DIRECTORY` constant definition to align with best practices and improve readability.

--- public/index.php
+++ public/index.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 use App\Sessions\Flash;
 
+session_start();
+
 define('PARENT_DIRECTORY', dirname(__DIR__));
 
-session_start();
 
 // import composer autoloader
 require_once PARENT_DIRECTORY . '/vendor/autoload.php';
