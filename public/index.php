55a164d2987aa0ee8533354c61d4fd5d24a0367b
AhmadOthman <trollingosman101@outlook.com>
2024-08-19 22:17:22 +0300

    refactor: update flash message clearing method
    - Replaced `Flash::flush()` with `Flash::clear()` to align with updated method names.

--- public/index.php
+++ public/index.php
@@ -8,11 +8,9 @@ session_start();
 
 define('PARENT_DIRECTORY', dirname(__DIR__));
 
-// import composer autoloader
 require_once PARENT_DIRECTORY . '/vendor/autoload.php';
 
 // bootstrap the application
 require_once PARENT_DIRECTORY . "/bootstrap/bootstrap.php";
 
-// flush any remaining flash messages
-Flash::flush();
+Flash::clear();
